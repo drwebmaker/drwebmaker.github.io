8af2c66466c4284140d20236e3ee27e419faeafc
valera <dr.webmaker@gmail.com>
2014-09-19 00:16:16 +0300

    222

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
 window.onload = function() {
-  var url = 'main.json';
-  var tbody = document.getElementById('table_key').getElementsByTagName("TBODY")[0];
+//  var url = 'main.json';
+//  var tbody = document.getElementById('table_key').getElementsByTagName("TBODY")[0];
+//
+//  var tr = document.createElement('TR');
+//  tr.innerHTML = '<td>1</td><td>2</td><td>3</td>';
+//
+//  tbody.appendChild(tr);
 
-  var tr = document.createElement('TR');
-  tr.innerHTML = '<td>1</td><td>2</td><td>3</td>';
+  var xmlhttp = new XMLHttpRequest();
+  var url = "main.json";
 
-  tbody.appendChild(tr);
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var myArr = JSON.parse(xmlhttp.responseText);
+      myFunction(myArr);
+    }
+  }
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+
+  function myFunction(arr) {
+
+    var i;
+    var out = "<table>";
+
+    for(i = 0; i < arr.length; i++) {
+      out += "<tr><td>" +
+        arr[i].key +
+        "</td><td>" +
+        arr[i].en +
+        "</td><td>" +
+        arr[i].nl +
+        "</td></tr>";
+    }
+    out += "</table>"
+    document.getElementById("table").innerHTML = out;
+  }
+
+
 };
 
